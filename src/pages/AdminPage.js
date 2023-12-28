0911a6c49829efc0468fae8f2f496f6885876b5e
seojonghon <seojonghon@naver.com>
2023-12-28 15:18:36 +0900

    RDS로변경과 차트 수정중 미완

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,11 +1,18 @@
+// AdminPage 컴포넌트
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Link } from 'react-router-dom';
+import AdminPage2 from './AdminPage2';  // AdminPage2 컴포넌트를 import
+import { Bar } from 'react-chartjs-2';
+import { Chart } from 'chart.js/auto';
+
 
 const AdminPage = ({ isAdmin }) => {
   const [data, setData] = useState(null);
-  const [companyUserCounts, setCompanyUserCounts] = useState({}); // 추가된 부분
+  const [companyUserCounts, setCompanyUserCounts] = useState({});
+  const [chart, setChart] = useState(null); // 차트에 대한 참조를 저장하는 상태 추가
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -26,20 +33,39 @@ const AdminPage = ({ isAdmin }) => {
         console.error('Error fetching data:', error);
       }
     };
-    fetchData();
-  }, []);
+
+    // 한 번만 실행되도록 변경
+    if (!data) {
+      fetchData();
+    }
+  }, [data]); // data를 의존성 배열에 추가
+
   const userRole = localStorage.getItem('role');
 
-  const renderTable = () => {
-    if (!isAdmin && userRole !== '1') {
-      return <p className="mt-4">You don't have permission to access this page.</p>;
+  const destroyChart = () => {
+    // 차트를 파괴하기 전에 확인
+    if (chart) {
+      chart.destroy();
     }
+  };
 
-    if (!data || !data.users) {
-      return <p className="mt-4">No data available</p>;
-    }
+  const createChart = () => {
+    // 차트 인스턴스 생성 및 참조 저장
+    const newChart = new Chart(/* 차트 옵션 설정 */);
+    setChart(newChart);
+  };
 
-    let filteredData };
+  useEffect(() => {
+    // 컴포넌트 마운트 시에 차트 생성
+    createChart();
+
+    // 컴포넌트 언마운트 시에 차트 파괴
+    return () => {
+      if (chart) {
+        chart.destroy();
+      }
+    };
+  }, []); // 빈 의존성 배열을 전달하여 한 번만 실행
 
   return (
     <div className="container mt-4">
@@ -53,8 +79,40 @@ const AdminPage = ({ isAdmin }) => {
             </p>
           ))}
 
-          {renderTable()}
-            <Link to="/" className="btn btn-primary mr-2">홈으로 이동</Link>
+          {/* 차트 추가 */}
+          <div className="mt-4">
+            <h4>기업별 유저 테이블 레코드 수</h4>
+            <Bar
+              data={{
+                labels: Object.keys(companyUserCounts),
+                datasets: [
+                  {
+                    label: '유저 수',
+                    data: Object.values(companyUserCounts),
+                    backgroundColor: 'rgba(75,192,192,0.4)',
+                    borderColor: 'rgba(75,192,192,1)',
+                    borderWidth: 1,
+                  },
+                ],
+              }}
+              options={{
+                scales: {
+                  x: {
+                    type: 'category', // "category" 스케일을 수동으로 등록
+                    title: { display: true, text: '기업' },
+                  },
+                  y: { title: { display: true, text: '유저 수' }, beginAtZero: true },
+                },
+              }}
+            />
+          </div>
+
+          {/* AdminPage2 컴포넌트를 렌더링하고 companyUserCounts prop을 전달 */}
+          <AdminPage2 isAdmin={isAdmin} companyUserCounts={companyUserCounts} />
+
+          <Link to="/" className="btn btn-primary mr-2" onClick={destroyChart}>
+            홈으로 이동
+          </Link>
         </>
       ) : (
         <p className="mt-4">You don't have permission to access this page.</p>
@@ -62,4 +120,5 @@ const AdminPage = ({ isAdmin }) => {
     </div>
   );
 };
+
 export default AdminPage;
--- src/pages/AdminPage2.js
+++ src/pages/AdminPage2.js
@@ -3,54 +3,51 @@ import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DonutChart from '../chart/DonutChart';
 import PChart from '../chart/PChart';
-
 import AiChart from '../chart/AiChart';
 import InfraChart from '../chart/InfraChart';
+import { Link } from 'react-router-dom';
+import AdminPage from './AdminPage';  // Import AdminPage2 component
 
-import { Link, useNavigate } from 'react-router-dom';
-
-const AdminPage2 = ({ isAdmin }) => {
-  const navigate = useNavigate();
-
+const AdminPage2 = ({ isAdmin , companyUserCounts }) => {
   const [data, setData] = useState(null);
   const [selectedTab, setSelectedTab] = useState('all');
   const [sortBy, setSortBy] = useState(null);
   const [sortOrder, setSortOrder] = useState('asc');
   const [DonutChartData, setDonutChartData] = useState({ win: 0, defeat: 0 });
   const [PChartData, setPChartData] = useState({ Delivery: 0, ECumus: 0, Other: 0 });
-
   const [AiChartData, setAiChartData] = useState({ a1: 0, a2: 0, a3: 0 });
-  const [InfraChartData, setInfraChartData] = useState({ i1: 0, i2: 0, i3: 0 });
+  const [InfraChartData, setInfraChartData] = useState({ Case1: 0, Case2: 0, Case3: 0 });
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:5000/api/get_data'); // Update API endpoint
+        const response = await axios.get('http://localhost:5000/api/get_data');
         setData(response.data);
-
+  
         const subCount = response.data.users.filter((user) => user.subscription_status === 'yes').length;
         const noSubCount = response.data.users.filter((user) => user.subscription_status === 'no').length;
-
+  
         const c1Count = response.data.users.filter((user) => user.category === 'Delivery').length;
         const c2Count = response.data.users.filter((user) => user.category === 'ECumus').length;
         const c3Count = response.data.users.filter((user) => user.category === 'Other').length;
-
+  
         const i1 = response.data.users.filter((user) => user.infraCategory === 'Case1').length;
         const i2 = response.data.users.filter((user) => user.infraCategory === 'Case2').length;
         const i3 = response.data.users.filter((user) => user.infraCategory === 'Case3').length;
-
+  
         const a1 = response.data.users.filter((user) => user.aiCategory === '일회 용기 세척 여부 AI').length;
         const a2 = response.data.users.filter((user) => user.aiCategory === '택배 테이프 제거 여부 AI').length;
         const a3 = response.data.users.filter((user) => user.aiCategory === '사람 인식 여부 AI').length;
-
+  
         setDonutChartData({ win: subCount, defeat: noSubCount });
         setPChartData({ Delivery: c1Count, ECumus: c2Count, Other: c3Count });
-        setAiChartData({a1: a1, a2: a2, a3: a3 })
-        setInfraChartData({Case1: i1, Case2: i2, Case3: i3 })
+        setAiChartData({ a1: a1, a2: a2, a3: a3 });
+        setInfraChartData({ Case1: i1, Case2: i2, Case3: i3 });
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
-
+  
     fetchData();
   }, []);
 
@@ -74,7 +71,7 @@ const AdminPage2 = ({ isAdmin }) => {
       const token = localStorage.getItem('token');
 
       // 서버에 사용자 삭제 요청
-      const response = await axios.delete(
+      await axios.delete(
         `http://localhost:5000/editor/customer/${email}`, // Update API endpoint
         {
           headers: {
@@ -83,12 +80,14 @@ const AdminPage2 = ({ isAdmin }) => {
         }
       );
 
-      console.log('Deleted User:', response.data);
+      console.log('Deleted User:', email);
 
       // 프론트엔드에서도 해당 사용자 삭제
-      const updatedData = { ...data };
-      updatedData.users = updatedData.users.filter((user) => user.email !== email);
-      setData(updatedData);
+      setData((prevData) => {
+        const updatedData = { ...prevData };
+        updatedData.users = updatedData.users.filter((user) => user.email !== email);
+        return updatedData;
+      });
     } catch (error) {
       console.error('Error deleting user:', error);
     }
@@ -169,7 +168,15 @@ const AdminPage2 = ({ isAdmin }) => {
     <div className="container mt-4">
       {isAdmin || userRole === '1' ? (
         <>
+          {/* 기업별 유저 수를 출력하는 부분 */}
+          {Object.keys(companyUserCounts).map((companyId) => (
+            <p key={companyId}>
+              {`기업 ${companyId}의 유저 테이블 레코드 수: ${companyUserCounts[companyId]}`}
+            </p>
+          ))}
+
           <h2 className="mb-4">최종 관리자 대시보드</h2>
+
           <div className="mb-4">
             <button className={`btn ${selectedTab === 'all' ? 'btn-primary' : 'btn-secondary'} mr-2`} onClick={() => handleTabClick('all')}>전체</button>
             <button className={`btn ${selectedTab === 'subscribed' ? 'btn-primary' : 'btn-secondary'} mr-2`} onClick={() => handleTabClick('subscribed')}>구독 중</button>
@@ -179,29 +186,30 @@ const AdminPage2 = ({ isAdmin }) => {
           </div>
 
           {renderTable()}
-          <div className="mt-4">
-  <div className="d-flex justify-content-between">
-    <div>
-      <h4>구독자 비율 차트</h4>
-      <DonutChart win={DonutChartData.win} defeat={DonutChartData.defeat} />
-    </div>
-    <div>
-      <h4>카테고리별 비율 차트</h4>
-      <PChart Delivery={PChartData.Delivery} ECumus={PChartData.ECumus} Other={PChartData.Other} />
-    </div>  
-  </div>
 
-  <div className="d-flex justify-content-between mt-4">
-    <div>
-      <h4>AI 구독 비율 차트</h4>
-      <AiChart a1={AiChartData.a1} a2={AiChartData.a2} a3={AiChartData.a3} />
-    </div>
-    <div>
-      <h4>인프라 선택 비율 차트</h4>
-      <InfraChart Case1={InfraChartData.Case1} Case2={InfraChartData.Case2} Case3={InfraChartData.Case3} />
-    </div>  
-  </div>
-</div>
+          <div className="mt-4">
+            <div className="d-flex justify-content-between">
+              <div>
+                <h4>구독자 비율 차트</h4>
+                <DonutChart win={DonutChartData.win} defeat={DonutChartData.defeat} />
+              </div>
+              <div>
+                <h4>카테고리별 비율 차트</h4>
+                <PChart Delivery={PChartData.Delivery} ECumus={PChartData.ECumus} Other={PChartData.Other} />
+              </div>
+            </div>
+
+            <div className="d-flex justify-content-between mt-4">
+              <div>
+                <h4>AI 구독 비율 차트</h4>
+                <AiChart a1={AiChartData.a1} a2={AiChartData.a2} a3={AiChartData.a3} />
+              </div>
+              <div>
+                <h4>인프라 선택 비율 차트</h4>
+                <InfraChart Case1={InfraChartData.Case1} Case2={InfraChartData.Case2} Case3={InfraChartData.Case3} />
+              </div>
+            </div>
+          </div>
 
           <div>
             <Link to="/" className="btn btn-primary mr-2">홈으로 이동</Link>
